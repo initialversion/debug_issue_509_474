272466c843a06b27d1835b95ec2531188ac5bbdf
Murugan <murugu2012@gmail.com>
2017-01-02 20:09:45 +0530

    Whitelist columns in Verify for ActiveAdmin

--- app/admin/verify.rb
+++ app/admin/verify.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Verify do
 
+ permit_params :user_id, :recruiting_event_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
