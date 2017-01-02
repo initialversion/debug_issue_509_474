9f91c17422da24bb1ee37cda3202e876f93f3567
Murugan <murugu2012@gmail.com>
2017-01-02 20:08:57 +0530

    Direct association between firms and recruiting_events

--- app/models/firm.rb
+++ app/models/firm.rb
@@ -1,6 +1,9 @@
 class Firm < ApplicationRecord
   # Direct associations
 
+  has_many   :recruiting_events,
+             :dependent => :destroy
+
   has_many   :target_firms,
              :dependent => :destroy
 
--- app/models/recruiting_event.rb
+++ app/models/recruiting_event.rb
@@ -1,6 +1,8 @@
 class RecruitingEvent < ApplicationRecord
   # Direct associations
 
+  belongs_to :firm
+
   # Indirect associations
 
   # Validations
