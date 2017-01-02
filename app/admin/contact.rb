4d01d47732e427a1c721a38237208e2d54b707d9
Murugan <murugu2012@gmail.com>
2017-01-02 20:09:32 +0530

    Whitelist columns in Contact for ActiveAdmin

--- app/admin/contact.rb
+++ app/admin/contact.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Contact do
 
+ permit_params :first_name, :last_name, :firm_id, :group, :division, :title, :photo, :business_card, :email, :phone_number
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
