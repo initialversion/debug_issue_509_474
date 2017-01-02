8bbbb3140d3f42c095c82cf7704cb87d6e1a2a55
Murugan <murugu2012@gmail.com>
2017-01-02 20:08:57 +0530

    Direct association between users and touchpoints

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :touchpoints,
+             :dependent => :destroy
+
   has_many   :follow_ups,
              :dependent => :destroy
 
