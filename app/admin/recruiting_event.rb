83d8a716d2920e6d3321d9bfc601a488d0f97440
Murugan <murugu2012@gmail.com>
2017-01-02 20:09:37 +0530

    Whitelist columns in RecruitingEvent for ActiveAdmin

--- app/admin/recruiting_event.rb
+++ app/admin/recruiting_event.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register RecruitingEvent do
 
+ permit_params :name, :location, :address, :date, :start_time, :end_time, :closed, :firm_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
