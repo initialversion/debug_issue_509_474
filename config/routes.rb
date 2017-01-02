91082de0a1f219285cbb0bb762e90179404f85a6
Murugan <murugu2012@gmail.com>
2017-01-02 20:09:18 +0530

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "groups#index"
   # Routes for the Milestone resource:
   # CREATE
