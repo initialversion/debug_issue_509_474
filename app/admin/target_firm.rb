e2a9254531ad9dade9de6397c22232c6c58dd8f1
Murugan <murugu2012@gmail.com>
2017-01-02 20:09:43 +0530

    Whitelist columns in TargetFirm for ActiveAdmin

--- app/admin/target_firm.rb
+++ app/admin/target_firm.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register TargetFirm do
 
+ permit_params :user_id, :firm_id, :touch_goal
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
